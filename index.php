dd22d29cc99cfa12300ddd13c1dcdf997eac6b63
Jordi Rosell <jroselln@gmail.com>
2019-01-09 20:31:36 +0100

    o-c.es website

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+<style>
+.content {padding:30px}
+</style>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <div class="content">
+		
+<div style="display:block;position:relative">
+<img src="logo.png">
+<h1 style="position:absolute;left:200px;top:0;">O-C Communication Systems</h1>
+</div>
+<ul><li>
+01.-Transmissió de Dades</li><li>
+02.-Sistemes Informàtics</li><li>
+03.-Sistemes  Unificats</li><li>
+04.-Radio Comunicació</li><li>
+05.-Xarxes</li><li>
+06.-Sistemes de Commutació
+</li></ul>
+<ul><li>
+1A.-Que es Comunicar</li><li>
+2A.-A Qui comuniquem ( La persona)</li><li>
+3A.-Que comuniquem (El Fet)</li><li>
+4A.-On Comuniquem( El Lloc)</li><li>
+5A.- Per quins Medis( Els Mitjans)</li><li>
+6A.-Perquè Comuniquem (Els Motius) </li><li>
+7A.-Com Comuniquem( La manera)</li><li>
+8A. -Quan comuniquem (El temps)
+</li></ul>
+<div>
+<h2>Tot esser vivent com organisme viu esta realitzant contínuament cinc funcions que li son necessàries per conservar-se y desenvolupar-se.</h2>
+<p>
+B1.-Obtenir informació sobre el seu medi, fent-se càrrec del que passa y pot passar al seu voltant, percebre les dificultats y facilitats amb graus que valora y  que el configuren en la seva vida en un moment donat.
+</p><p>
+B2.-A partir de la informació, que li arribat pels seus sentits, elabora la resposta mes favorable per ell, adopta la conducta adequada, segons les seves capacitats de actuació.
+</p><p>
+B3.-Posar en pràctica la seva resposta, la pròpia actuació. 
+</p><p>
+B4.-Percebre els resultats obtinguts amb la seva actuació, valorant el grau d’èxit i possible fracàs que ha assolit.
+</p><p>
+B5.-Modificar la seva conducta en funció del grau d’eficàcia  obtinguts amb actes anteriors. S`han dona conta de la seva situació al medi i de las possibilitats que te d’actuar de les seves habilitats, intuïcions , idees o valors,  amb la nova informació de  l’experiència acumulada y acabada de rebre. 
+</p><p>
+B6.-Dues limitacions de comunicar que tenen el essers humans
+</p><p>
+B7.-D’individu només pot comunicar a partir del seu propi sistema de significacions, conjunt d’estructures afectives i intel·lectuals que orienten la percepció del món i les formes habituals d’actuar-hi.
+</p><p>
+B8.-La comunicació sols es pot establir mitjançant l’ús d’un llenguatge; i tots els llenguatges realitzen necessariament, una simplificació de la realitat.
+OC - ??Communication Systems www. o-c.es E-mail: contacto@o-c.es
+</p> 
+<p>
+B9.-El Procés de la Comunicació. 
+La comunicació és l'intercanvi de coneixements i vivències entre persones a través de diferents llenguatges. 
+La comunicació suposa informació d'un punt (persona o entitat) a un altre. Consisteix en missatges, amb un cert contingut i format, que l'emissor envia al receptor.
+Un objectiu important de la comunicació és provocar una acció, de vegades unidireccional, tot i que sovint implica una interacció entre les parts que es comuniquen. També en aquest cas la comunicació pot servir de punt de partida per a l'acció. (Share)
+</p>
+</div>
+<div class="content">
+<h1>1a.-Que és Comunicar</h1>
+<ul><li>
+1A.1/B.-Funciones necessàries i limitacions</li><li>
+1A-2/C.-Elementos de la Comunicació</li><li>
+1A-3/D.-Funciones del Llenguatge</li><li>
+1A-4/E.-El Signe</li><li>
+1A-5/F.- El Codi</li><li>
+1A-6/G.- El Significat</li><li>
+1A-7/H.- Codi Oral</li><li>
+1A-8/J.- Codi Escrit</li><li>
+1A-9/K.- La Paraula
+</li></ul>
+<p>
+Existeixen nombroses formes de comunicació en la natura entre animals vius. Dues persones comuniquen entre si per la veu i l'oïda, els gestos i la vista, les olors i l'olfacte, el tacte entre ambdós o els símbols gràfics. Des de sempre l'home ha dut a terme la comunicació a llarga distància amb mitjans com els sons de tambor, senyals de fum, coloms missatgers, campanes i senyals lluminoses. Avui dia, aquestes formes de comunicació han quedat ampliades i superades per la comunicació elèctrica.Això es deu al fet que es poden transmetre els senyals elèctrics a distàncies molt grans, teòricament a qualsevol distància de l'univers i amb una velocitat molt alta 3x108 metres per segon aproximadament).
+</p>
+<h2>1A-1/B.- Funcions necessàries i limitacions</h2>
+<p>
+Tot ésser vivent com a organisme viu aquesta contínuament realitzant cinc funcions que li són necessàries per a conservar i desenvolupar-se.
+</p>
+<p>La comunicació suposa informació d'un punt (persona o entitat) a un altre. Consisteix en missatges, amb un cert contingut i format, que l'emissor envia al receptor.
+Un objectiu important de la comunicació és provocar una acció, de vegades unidireccional, tot i que sovint implica una interacció entre les parts que es comuniquen. També en aquest cas la comunicació pot servir de punt de partida per a l'acció. (Share)
+</p>
+
+<h2>1A-2/C.- Elements de la Comunicació</h2>
+<ul><li>
+C1.-Emissor: Qui codifica el missatge</li><li>
+C2.-Receptor: el que el rep i descodifica.</li><li>
+C3.-Referència: Allò del que es parla.</li><li>
+C4 .- Missatge: Expressió formal codificada.</li><li>
+C5.-Canal: Mitjà de transmissió.</li><li>
+C6.-Codi: Sistema de signes i regles.
+</li></ul>
+<h2>1A-3/D.- Funcions del Llenguatge</h2>
+<ul><li>
+D1.-Emotiva: L'interès de la comunicació es centra en l'emissor.</li><li>
+D2.-Conativa: S'orienta cap al receptor.</li><li>
+D3.-Referencial: Remet a la realitat.</li><li>
+D4.-Poètica: Se centra en la forma del missatge.</li><li>
+D5.-fàctica. Comprova el funcionament del canal de transmissió.</li><li>
+D6.-Metalingüística: Centra la comunicació en el codi.
+</li></ul>
+<p>
+Aquestes funcions no solen donar-se aïllades en els missatges: normalment hi ha una predominant i altres secundàries. Les més freqüents són les tres primeres; La quarta és pròpia del llenguatge literari.
+</p>
+<h2>1A-4/E.- El Signe</h2>
+<ul><li>
+E1.-Signe: És un element, perceptible pels sentits, que substitueix a un altre.</li><li>
+E2.-Classes de Signes:</li><li>
+E3.-Índexs: El signe té una relació física o de causa efecte amb l'element al qual substitueix.</li><li>
+E4.-Icones: El signe manté una relació de semblança amb l'objecte representat.</li><li>
+E5.-Símbols: La relació entre signe i objecte és arbitrària, fruit d'un acord entre els qui l'utilitzen.
+</li></ul>
+<h2>1A-5/F.- El Codi</h2>
+<ul><li>
+F1.-Codi: Conjunt de signes. N'hi ha lingüístics i no lingüístics.</li><li>
+F2 .- Signe lingüístic: és el resultant de la unió entre una cadena de fonemes (significant, part material) i el concepte o la idea que representa el signe (significat).</li><li>
+F3 .- Característiques del signe lingüístic.</li><li>
+F4.-Arbitrarietat: és fruit d'una convenció, d'un acord entre els seus usuaris. És doncs, un signe simbòlic.</li><li>
+F5.-Lineal: Es desenvolupa en el temps i els seus elements apareixen ordenats un rere l'altre.</li><li>
+F6.-Immutables: El parlant no pot variar a voluntat doncs és patrimoni de la comunitat de parlants.</li><li>
+F7.-Doble articulació: Elements mínims sense significat (fonemes) configuren la segona articulació, i la seva combinació forma els lexemes i morfemes que si tenen significat.
+</li></ul>
+<h2>1A-6/G.- El Significat</h2>
+<ul><li>
+G1.-Semàntica: Ciència que estudia el significat.</li><li>
+G2.-Denotació: Significat objectiu de paraules i expressions</li><li>
+G3.-Connotació: Significat subjectiu, suggerit, suggerent, condicionat pel context de la situació</li><li>
+G4.-monosèmia: Un significant, un significat.</li><li>
+G5.-Homonímia: Significant igual, significat diferent. Origen diferent, n'hi ha homòfones i homògrafes.</li><li>
+G6.-Sinonímia: Diferent significant, mateix o semblant significat.</li><li>
+G7.-antonímia. Significats oposats.</li><li>
+G8.-hiperonímia: Paraules de significat general respecte a altres del seu mateix camp semàntic.
+G9.-Singlot nímia. Paraules de significat reduït respecte a altres del seu mateix camp semàntic.</li><li>
+G10.-Camp semàntic: Paraules d'una mateixa categoria gramatical, alguns semes comuns i altres diferents.</li><li>
+G11.-Canvis semàntics: Canvis de significació de les paraules per ampliació, reducció o adopció d'un significat nou. Es deuen a causes lingüístiques, històriques, socials, (tabú i eufemisme) o psicològiques.
+</li></ul>
+<h2>1A-7/H.- Codi Oral</h2>
+<ul><li>
+H1.-Aprenentatge espontani</li><li>
+H2.-Canal auditiu</li><li>
+H3.-Variants dialectals i socials</li><li>
+H4.-Presència física dels interlocutors.</li><li>
+H5.-Descuit en la selecció i organització d'idees.v
+H6.-Sintaxi senzilla.</li><li>
+H7.-Lèxic poc específic</li><li>
+H8.-Immediatesa.</li><li>
+H9.-Suport de codis no verbals.</li><li>
+H10.-Paral·lenguatje: To, entonació, silencis.</li><li>
+H11.-Kinèsica: Gestos i expressió.</li><li>
+H12.-Proxèmica. Postura i distància entre els interlocutors.
+</li></ul>
+<h2>1A-8/J.-Código Escrit</h2>
+<ul><li>
+J1.-Aprenentatge racional.</li><li>
+J2.-Canal visual.</li><li>
+J3.-Vehicle del coneixement i del saber.</li><li>
+J4.-Sistematització d'idees: Connectors, puntuació.</li><li>
+J5.-Complexitat sintàctica.</li><li>
+J6.-Comunicació diferida en el temps i en l'espai
+</li></ul>
+<p>Els avenços tecnològics han aproximat ambdós codis. De vegades utilitzem fórmules intermèdies (gènere epistolar, notes informatives)</p>
+<h2>1A-9/K.-La Paraula</h2>
+<ul><li>
+K1.-La Paraula és un do. Dóna capacitat d'estimar, de relacionar-nos, per consolar, per aconsellar, per ensenyar, per pregar.</li><li>
+K2.-La paraula ens serveix per a l'amistat, per a la pau, per al perdó, per a la comunicació entre els homes.</li><li>
+K3.-La paraula està al servei de les grans obres, d'expressar la felicitat de l'home, d'anunciar i donar testimoni de la Veritat. No només estem fets per dir la veritat sinó que estem preparats per a reconèixer-la. Hi ha una ètica i una raó darrere de cada paraula. No es pot dir qualsevol cosa.</li><li>
+K4.-La paraula i el seu significat: El que hi ha darrere de cada paraula, el significat, està en l'amor, la sinceritat, l'honestedat o la benevolència que hi ha al cor del que la pronuncia.
+</li></ul>
+
+		</div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
